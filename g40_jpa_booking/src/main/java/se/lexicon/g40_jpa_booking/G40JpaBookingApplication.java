7ee8651e6ff06c301ae49898ace1889cffd23b2e
Simon Elbrink <simon.elbrink@gmail.com>
2022-02-21 09:38:58 +0100

    Initial commit

---
+++ g40_jpa_booking/src/main/java/se/lexicon/g40_jpa_booking/G40JpaBookingApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g40_jpa_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G40JpaBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G40JpaBookingApplication.class, args);
+	}
+
+}
---
+++ g40_jpa_booking/src/test/java/se/lexicon/g40_jpa_booking/G40JpaBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g40_jpa_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G40JpaBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
