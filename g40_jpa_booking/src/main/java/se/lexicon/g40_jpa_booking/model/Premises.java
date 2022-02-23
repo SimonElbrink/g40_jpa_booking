6fde53636eb7b9550b0839cb3481723cd6a3c30d
Simon Elbrink <simon.elbrink@gmail.com>
2022-02-23 09:58:13 +0100

    Added OneToMany Relationship with Premises -> Booking + convenience Methods to handle relationship from Premises

--- g40_jpa_booking/src/main/java/se/lexicon/g40_jpa_booking/model/Premises.java
+++ g40_jpa_booking/src/main/java/se/lexicon/g40_jpa_booking/model/Premises.java
@@ -3,6 +3,8 @@ package se.lexicon.g40_jpa_booking.model;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static se.lexicon.g40_jpa_booking.model.EntityConstants.GENERATOR;
@@ -16,14 +18,25 @@ public class Premises {
     @GenericGenerator(name = GENERATOR, strategy = UUID_GENERATOR)
     @Column(updatable = false)
     private String id;
+
     private String name;
+
     @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "fk_address_id")
     private Address address;
 
+    @OneToMany(
+            cascade = {CascadeType.DETACH, CascadeType.REFRESH, CascadeType.MERGE, CascadeType.PERSIST},
+            fetch = FetchType.LAZY,
+            orphanRemoval = true,
+            mappedBy = "premises"
+    )
+    private List<Booking> bookings;
+
     public Premises(String id, String name) {
         this.id = id;
         this.name = name;
+        this.bookings = new ArrayList<>();
     }
 
     public Premises() {
@@ -51,6 +64,51 @@ public class Premises {
 
     public void setAddress(Address address) {
         this.address = address;
+    }
+
+    public List<Booking> getBookings() {
+        if(this.bookings == null) this.bookings = new ArrayList<>();
+        return bookings;
+    }
+
+    //TODO Handle Setter in a bidirectional way.
+    // Make sure to set relationships of both ends
+    // Empty we need to remove relationships
+    // Or set the relationships.
+    public void setBookings(List<Booking> bookings) {
+        if (bookings == null) bookings = new ArrayList<>();
+
+        if (bookings.isEmpty()){
+            if (this.bookings != null){
+                this.bookings.forEach(booking -> booking.setPremises(null));
+            }
+        }else{
+            bookings.forEach(booking -> booking.setPremises(this));
+        }
+        this.bookings = bookings;
+    }
+
+
+    //Convenience Method
+    public void addBooking(Booking booking){
+        if (booking == null) throw new IllegalArgumentException("Booking was null");
+        if (this.bookings == null) this.bookings = new ArrayList<>();
+
+        if (!this.bookings.contains(booking)){
+            this.bookings.add(booking);
+            booking.setPremises(this);
+        }
+    }
+
+    //Convenience Method
+    public void removeBooking(Booking booking){
+        if (booking == null) throw new IllegalArgumentException("Booking was null");
+        if (this.bookings == null) this.bookings = new ArrayList<>();
+
+        if (this.bookings.contains(booking)){
+            this.bookings.remove(booking);
+            booking.setPremises(null);
+        }
     }
 
     @Override
