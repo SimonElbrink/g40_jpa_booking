71fb381c23ae9cae16c24fda64a43a577a5b05ca
Simon Elbrink <simon.elbrink@gmail.com>
2022-03-02 11:23:59 +0100

    ContactInfoEntityService Implemented!

---
+++ g40_jpa_booking/src/main/java/se/lexicon/g40_jpa_booking/service/entity/ContactInfoEntityService.java
@@ -0,0 +1,7 @@
+package se.lexicon.g40_jpa_booking.service.entity;
+
+import se.lexicon.g40_jpa_booking.model.dto.form.ContactInfoForm;
+import se.lexicon.g40_jpa_booking.model.entity.ContactInfo;
+
+public interface ContactInfoEntityService extends GenericEntityService<ContactInfo,String, ContactInfoForm>{
+}
