0dcd42c2ab8614aea4bf4a20ac259a0623cc9c42
Simon Elbrink <simon.elbrink@gmail.com>
2022-03-21 14:58:10 +0100

    First controller Created.

---
+++ g40_jpa_booking/src/main/java/se/lexicon/g40_jpa_booking/controller/TestController.java
@@ -0,0 +1,16 @@
+package se.lexicon.g40_jpa_booking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String helloMessage(@RequestParam(value = "message", defaultValue = "Hello World!") String customMessage){
+        return customMessage;
+    }
+
+}
